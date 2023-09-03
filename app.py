91bf38c788c4cc8f7266f8a9658e1d7b81ff7be3
Abhishek Shah <www.abhishekshah007@gmail.com>
2023-09-03 09:59:34 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def careers():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ gunicorn-config.py
@@ -0,0 +1,4 @@
+# Adjust the number of worker processes as needed
+workers = 4
+# Replace with the desired host and port
+bind = "0.0.0.0:8000"  
